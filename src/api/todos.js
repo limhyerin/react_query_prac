4038df2ed3017f2e9766d089fbcf2021b13db1ad
limhyerin <rin091345@gmail.com>
2024-02-19 11:41:31 +0900

    first commit

---
+++ src/api/todos.js
@@ -0,0 +1,15 @@
+// axios 요청이 들어가는 모든 모듈
+import axios from "axios";
+
+const getTodos = async () => {
+  const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/todos`);
+  console.log(response);
+  return response;
+};
+
+// 추가
+const addTodo = async (newTodo) => {
+  await axios.post(`${process.env.REACT_APP_SERVER_URL}/todos`, newTodo);
+};
+
+export { getTodos, addTodo };
--- src/redux/components/Input/Input.jsx
+++ src/redux/components/Input/Input.jsx
@@ -8,7 +8,8 @@ import "./styles";
 import { StyledDiv } from "./styles";
 import { useDispatch, useSelector } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
-import { addTodo } from "../../modules/todos";
+import { addTodo } from "../../../api/todos";
+import { useMutation, useQueryClient } from "react-query";
 
 /**
  * 컴포넌트 개요 : Todo 메인 페이지에서 제목과 내용을 입력하는 영역
@@ -18,6 +19,15 @@ import { addTodo } from "../../modules/todos";
  */
 function Input() {
   const dispatch = useDispatch();
+
+  // 리액트 뭐리 관련 코드
+  const queryClient = useQueryClient();
+  const mutation = useMutation(addTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+      console.log('성공하였습니다');
+    }
+  })
 
   // useSelector를 통한, store의 값 접근
   const todos = useSelector((state) => state.todos);
@@ -83,7 +93,9 @@ function Input() {
 
     // todo를 추가하는 reducer 호출
     // 인자 : payload
-    dispatch(addTodo(newTodo));
+    // dispatch(addTodo(newTodo));
+
+    mutation.mutate(newTodo);
 
     // state 두 개를 초기화
     setTitle("");
